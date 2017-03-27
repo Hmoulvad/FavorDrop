6fe6e643a2fa36c570dda06f2c1b3fcfc6ec655f
madswerner <mads_werner@hotmail.com>
2017-03-27 15:30:47 +0200

    Jeg har rydet lidt op i login/signup

--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from "@angular/forms";
+import {moveIn} from "../router.animations";
+import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'fd-user-login',
   templateUrl: './user-login.component.html',
-  styleUrls: ['./user-login.component.css']
+  styleUrls: ['./user-login.component.css'],
+  animations: [moveIn()],
+  host: {'[@moveIn]': ''}
 })
 export class UserLoginComponent{
 
@@ -19,7 +24,41 @@ export class UserLoginComponent{
     window.alert(this.user.name + "\n" + this.user.lastname);
   }
 
-  constructor() { }
+  error : any;
+
+  constructor(public af: AngularFire, private router: Router) {
+    this.af.auth.subscribe(auth => {
+      if(auth){
+        this.router.navigateByUrl('/members')
+      }
+    });
+  }
+
+  loginFb() {
+    this.af.auth.login({
+      provider: AuthProviders.Facebook,
+      method: AuthMethods.Popup,
+    }).then(
+      (success) => {
+        this.router.navigate(['/members']);
+      }).catch(
+      (err) => {
+        this.error = err;
+      })
+  }
+
+  loginGoogle() {
+    this.af.auth.login({
+      provider: AuthProviders.Google,
+      method: AuthMethods.Popup,
+    }).then(
+      (success) => {
+        this.router.navigate(['/members']);
+      }).catch(
+      (err) => {
+        this.error = err;
+      })
+  }
 
 
 }
