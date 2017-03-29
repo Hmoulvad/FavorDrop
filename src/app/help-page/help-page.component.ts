45b0d8116e042dbef28e6c2c197ae7ab3dccb7c6
TheDave <davidjkristensen@gmail.com>
2017-03-29 12:09:44 +0200

    Help Component added

---
+++ src/app/help-page/help-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fd-help-page',
+  templateUrl: './help-page.component.html',
+  styleUrls: ['./help-page.component.css']
+})
+export class HelpPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
