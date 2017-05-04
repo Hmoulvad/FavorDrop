7aa13af0bb41b230edd737ee6ce5da4230ce0273
TheDave <davidjkristensen@gmail.com>
2017-05-04 16:59:55 +0200

    Google Maps tilføjet

--- src/app/order-module/order.routes.ts
+++ src/app/order-module/order.routes.ts
@@ -4,5 +4,5 @@ import {OrderDetailComponent} from "./order-detail/order-detail.component";
 
 export const ORDER_ROUTES: Routes = [
   { path: '', component: OrderStartComponent },
-  { path: ':id', component: OrderDetailComponent },
-]
+  { path: ':id', component: OrderDetailComponent }
+];
