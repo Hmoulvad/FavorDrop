243fa2cf523d4848188ac4e48258754af1ddd97f
TheDave <davidjkristensen@gmail.com>
2017-05-05 19:05:50 +0200

    order routes fjernet.. bliver ikke brugt

--- src/app/order-module/order.routes.ts
+++
@@ -1,6 +0,0 @@
-import {Routes} from "@angular/router";
-import {OrderDetailComponent} from "./order-detail/order-detail.component";
-
-export const ORDER_ROUTES: Routes = [
-  { path: ':id', component: OrderDetailComponent }
-]
